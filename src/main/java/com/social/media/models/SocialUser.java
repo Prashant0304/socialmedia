ae00f6815c01769ea3b41091d3ca8961b3e464be
Prashant0304 <prashantpattanashetti5@gmail.com>
2025-09-09 15:18:30 +0530

    Created APIs

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,19 +1,27 @@
 package com.social.media.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SocialUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     @OneToOne(mappedBy = "user")
-//    @JoinColumn(name="social_profile_id")
+    //@JoinColumn(name = "social_profile_id")
+    @JsonIgnore
     private SocialProfile socialProfile;
 
     @OneToMany(mappedBy = "socialUser")
@@ -24,6 +32,11 @@ public class SocialUser {
             name = "user_group",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "group_id")
-            )
+    )
     private Set<SocialGroup> groups = new HashSet<>();
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
 }
